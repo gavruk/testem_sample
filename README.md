1f25b4fcb8a1aee2b04f09a1bf5e59dfc5fec1d2
Sergey Gavruk <svgavruk@gmail.com>
2016-03-09 21:53:31 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Setup
+
+First install dependencies
+
+    npm install
+
+Then, just run
+
+    node ./node_modules/testem/testem.js
---
+++ tests/code/tests.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var hello = require('../../lib/code/code');
+
+describe('hello', function(){
+    it('says hello world', function(done){
+        assert.equal(hello(), 'hello world');
+        setTimeout(done, 400);
+    });
+    it('says hello to person', function(done){
+        assert.equal(hello('Bob'), 'hello Bob');
+        setTimeout(done, 400);
+    });
+});
